1d177a62e80ad326ed58b9da2bca6e5c3e6238d0
KociOrges <koci.orges@hotmail.com>
2025-12-15 12:59:24 +0000

    Fix packaging: use find_packages and clean deps

--- setup.py
+++ setup.py
@@ -1,61 +1,33 @@
-import os
 import pathlib
-from setuptools import setup
+from setuptools import setup, find_packages
 from ait.commons.util.settings import NAME, VERSION, DESC, AUTHOR, AUTHOR_EMAIL
 
-# Directory containing this file
 HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+INSTALL_REQS = (HERE / "requirements.txt").read_text().splitlines()
 
-# Text of the README file
-README = (HERE / 'README.md').read_text()
-
-# Install requirements from requirements.txt
-INSTALL_REQS = [line.rstrip() for line in open(os.path.join(os.path.dirname(__file__), 'requirements.txt'))]
-
-# Additional install requirements
-ADDITIONAL_REQS = [
-    'boto3>=1.23.10',
-    'botocore>=1.26.10',
-    'filetype==1.0.7',
-    'requests>=2.20.0, <3',
-    'urllib3<1.27, >=1.25.4',
-    'tqdm~=4.57.0',
-    'pandas~=2.2.2',
-    'setuptools~=59.6.0',
-    'openpyxl==3.1.3'
-]
-
-# Combine the install requirements
-ALL_REQS = INSTALL_REQS + ADDITIONAL_REQS
-
-# This call to setup() does all the work
 setup(
-    # dashes are ok in repo and PyPI dist names but not in package (i.e. directory) and
-    # module (.py file) names. can't do import xyz-abc
     name=NAME,
     version=VERSION,
     description=DESC,
     long_description=README,
-    long_description_content_type='text/markdown',
-    url='https://github.com/ebi-ait/morphic-util',
+    long_description_content_type="text/markdown",
+    url="https://github.com/ebi-ait/morphic-util",
     author=AUTHOR,
     author_email=AUTHOR_EMAIL,
-    license='Apache License',
-    python_requires='>=3.10',
+    license="Apache License",
+    python_requires=">=3.10",
     classifiers=[
-        'License :: OSI Approved :: Apache Software License',
-        'Operating System :: MacOS :: MacOS X',
-        'Operating System :: POSIX',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
     ],
-    platforms=['MacOS X', 'Posix'],
-    packages=find_packages(include=['ait*']),
+    packages=find_packages(include=["ait*"]),
     include_package_data=True,
-    install_requires=ALL_REQS,
-    entry_points={
-        'console_scripts': [
-            f'{NAME}=ait.commons.util.__main__:main',
-        ]
-    },
+    install_requires=[r for r in INSTALL_REQS if r.strip() and not r.strip().startswith("#")],
+    entry_points={"console_scripts": [f"{NAME}=ait.commons.util.__main__:main"]},
 )
+
